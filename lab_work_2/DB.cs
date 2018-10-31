7872b732a8fe3a8854fe45a1feb8517e34122aee
saxumcordis <victor123952@gmail.com>
2018-10-31 23:21:18 +0300

    init

---
+++ lab_work_2/DB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace lab_work_2
+{
+    public class DB
+    {
+        private static readonly DB instance = new DB();
+
+        private readonly MySqlConnection db;
+        private DB()
+        {
+            db = new MySqlConnection("Database=CLASSROOM;Data Source=localhost;User Id=root;Password=");
+        }
+
+
+        public static List<object[]> execute(string query)
+        {
+            var command = new MySqlCommand(query, instance.db);
+
+            instance.db.Open();
+
+            var result = new List<object[]>();
+
+
+
+
+            for (var reader = command.ExecuteReader(); reader.Read();)
+            {
+                var arr = new object[reader.FieldCount];
+                reader.GetValues(arr);
+                result.Add(arr);
+            }
+
+
+
+            instance.db.Close();
+            return result;
+        }
+        public void read(string query) { }
+    }
+}
---
+++ lab_work_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace lab_work_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DB.execute("DELETE FROM students WHERE id = 1");
+            DB.execute("UPDATE students SET name = 'имя2' WHERE id = 2");
+            // DB.execute("INSERT INTO students (name, birth, university, class, level, gpa) VALUES('tolek plakat xochet', '1901', 'memos', 'sbv', 1, 1)");
+            Console.ReadKey();
+        }
+
+
+        
+
+    }
+}
