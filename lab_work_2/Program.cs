50fcb7c8e8cfc8e89757f9507aea10ba206634ba
saxumcordis <victor123952@gmail.com>
2018-11-05 16:36:24 +0300

    Init

--- lab_work_2/Program.cs
+++ lab_work_2/Program.cs
@@ -12,10 +12,7 @@ namespace lab_work_2
     {
         static void Main(string[] args)
         {
-            // DB.execute("DELETE FROM students WHERE id = 1");
-            DB.execute("UPDATE students SET name = 'имя2' WHERE id = 2");
-            // DB.execute("INSERT INTO students (name, birth, university, class, level, gpa) VALUES('tolek plakat xochet', '1901', 'memos', 'sbv', 1, 1)");
-            Console.ReadKey();
+            StudentsRepository.delete(2);
         }
 
 
---
+++ lab_work_2/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_work_2
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string birth { get; set; }
+        public string university { get; set; }
+        public string classString { get; set; }
+        public int level { get; set; }
+        public int gpa { get; set; }
+
+        public Student() { }
+
+        public Student(int id, string name, string birth, string university,
+            string classString, int level, int gpa) {
+            this.id = id;
+            this.name = name;
+            this.birth = birth;
+            this.university = university;
+            this.classString = classString;
+            this.level = level;
+            this.gpa = gpa;
+            }
+
+    }
+
+}
+    
